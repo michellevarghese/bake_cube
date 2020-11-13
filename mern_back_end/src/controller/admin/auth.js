70f3cac15315301618b3d2fe97c8fc9dc9b46026
michaela <hey@gmail.com>
2020-11-13 07:03:59 +0530

    api reques validation

--- mern_back_end/src/controller/admin/auth.js
+++ mern_back_end/src/controller/admin/auth.js
@@ -1,8 +1,10 @@
 const user = require('../../models/user');
 const User=require('../../models/user');
 const jwt= require('jsonwebtoken');
+const {validationResult} =require('express-validator');
 
 exports.signup=(req,res)=>{
+    
     
     User.findOne({email:req.body.email})
     .exec((error,user)=>{
@@ -64,10 +66,3 @@ exports.signin = (req, res) => {
     });
 }
 
-exports.requireSignin=(req,res,next)=>{
-    const token=req.headers.authorization.split(" ")[1];
-    const user=jwt.verify(token,process.env.JWT_SECRET);
-    req.user=user;
-
-    next();
-}
